1e2679844fe6af5b701824a34f0346fbea6a36af
Estudante <trodovalhogoncalves@gmail.com>
2026-03-04 11:20:12 -0400

    hent

--- funcoes/atv5.js
+++ funcoes/atv5.js
@@ -1,15 +1,12 @@
-const a = require('a');
-const readline = require('readline-sync');
 let pares = 0;
-let count = 1;
 let mat = [
     1,2,
     3,4
 ];
 
 function contarParesMatriz(){
-    for(let i = 0; i < 1; i++){
-        for (let j = 0; j < 4; j++) {
+    for(let i = 0; i < mat.length; i++){
+        for (let j = 0; j < mat[i].length; j++) {
             if(parseInt(mat[i,j]) % 2 == 0){
                 pares += 1;
             }
@@ -20,8 +17,8 @@ function contarParesMatriz(){
 
 pares = contarParesMatriz();
 
-for(let i = 0; i < 1; i++){
-    for (let j = 0; j < 4; j++) {
+for(let i = 0; i < mat.length; i++){
+    for (let j = 0; j < mat[i].length; j++) {
         console.log(mat[i,j]);
     }
 }
